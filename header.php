6eea91eca83d65df03a6748cc46945236888062e
Nikola <djindjojlo@gmail.com>
2020-11-14 12:48:54 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML><html><head>
+	<title>Porodično stablo porodica Marković, Marinković i Glišić</title>
+	<meta charset="utf-8" />
+<!-- for Google -->
+    <meta name="description" content="Porodično stablo porodica Marković, Marinković i Glišić" />
+    <meta name="keywords" content="porodicno, porodično, stablo, chart, d3js chart, genealogy, tree, circle" />
+    <meta name="author" content="Nikola Marković" />
+<!-- for Facebook -->
+    <meta property="og:title" content="Porodično stablo porodica Marković, Marinković i Glišić" />
+    <meta property="og:type" content="article" />
+    <meta property="og:locale" content="sr_RS" /> 
+    <meta property="og:image" content="http://markovic.ml/fb.png" />
+    <meta property="og:url" content="http://markovic.ml/" />
+    <meta property="fb:profile_id" content="1462785273" />
+    <meta property="article:author" content="https://www.facebook.com/nikola.markovic" />
+    <meta property="og:description" content="Porodično stablo porodica Marković, Marinković i Glišić" />
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+	<link rel="stylesheet" href="assets/css/main.css" />	
+	<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	
+	<script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" charset="utf-8"></script>
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans&amp;subset=latin,latin-ext" rel="stylesheet" type="text/css">
+	
+	<style  type='text/css'>
+
+ .node circle {
+   fill: #fff;
+   stroke: steelblue;
+   stroke-width: 3px;
+ }
+
+ .node text { font: 12px sans-serif; }
+
+ .link {
+   fill: none;
+   stroke: #ccc;
+   stroke-width: 2px;
+ }
+    </style>
+	</head><body>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
