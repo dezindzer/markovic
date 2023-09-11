0fc2956d9413e8bfb3fe4f46b7893f6b19108674
Nikola <djindjojlo@gmail.com>
2023-09-11 23:59:37 +0200

    aaa

--- assets/js/funkcija.js
+++ assets/js/funkcija.js
@@ -30,7 +30,7 @@ $(document).mousemove(function(event){
             padding = 5,
             duration = 1500;
 
-    var color = d3.schemeCategory20();
+    var color = d3.scaleOrdinal(d3.schemeCategory20c);
 
     var div = d3.select("#vis");
 
